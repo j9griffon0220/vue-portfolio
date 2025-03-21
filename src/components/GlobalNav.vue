13485e84dd118d7be91076f0a2891b7b56ac8063
Ayako Nakayama <j9griffon0220@gmail.com>
2025-03-21 19:44:09 +0900

    GlobalNavのcomponentsを作成

---
+++ src/components/GlobalNav.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { RouterLink } from 'vue-router'
+</script>
+
+<template>
+    <nav>
+        <ul>
+            <li><RouterLink to="/">Home</RouterLink></li>
+            <li><RouterLink to="/about">About</RouterLink></li>
+            <li><RouterLink to="/Skills">Skills</RouterLink></li>
+            <li><RouterLink to="/works">works</RouterLink></li>
+            <li><RouterLink to="/note">note</RouterLink></li>
+        </ul>
+    </nav>
+</template>
+
+<style scoped>
+</style>
