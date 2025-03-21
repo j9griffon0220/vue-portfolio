58cc51be98ed0bf8be0930765d4d87ba7f4ff876
Ayako Nakayama <j9griffon0220@gmail.com>
2025-03-21 20:17:15 +0900

    importするときのパスで@を使うための記述追加

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // TailwindのViteプラグインをインポート
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,4 +10,9 @@ export default defineConfig({
     vue(),
     tailwindcss(), // ← Tailwindのプラグインを追加
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
 })
