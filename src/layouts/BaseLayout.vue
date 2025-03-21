cd461dae5202e9baa31d7fbe46a60433fb1cd304
Ayako Nakayama <j9griffon0220@gmail.com>
2025-03-21 20:04:33 +0900

    layoutsにページのベースとなるBaseLayoutを作成

---
+++ src/layouts/BaseLayout.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+</script>
+
+<template>
+    <Header />
+    <main>
+        <div>
+            <!-- ここに各ページの左エリアが入る -->
+            <slot name="left"></slot>
+        </div>
+        <div>
+            <!-- ここに各ページの右エリアが入る -->
+            <slot name="right"></slot>
+        </div>
+    </main>
+    <Footer />
+</template>
