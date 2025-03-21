c2031eef7bbc35d57af3245a0c334f9105f9a252
Ayako Nakayama <j9griffon0220@gmail.com>
2025-03-21 20:02:01 +0900

    Skillsページのviewsファイルを作成

---
+++ src/views/Skills.vue
@@ -0,0 +1,26 @@
+<script setup>
+import BaseLayout from '@/layouts/BaseLayout.vue'
+import { useHead } from '@vueuse/head'
+
+useHead({
+    title: 'Skills | Ayako Nakayama portfolio',
+    meta: [
+        {name: 'description', content: 'スキル紹介ページです。'},
+        {property: 'og:title', content: 'Skills | Ayako Nakayama portfolio'},
+        {property: 'og:url', content: 'https://your-portfolio.com/skills'}
+    ]
+})
+</script>
+
+<template>
+    <BaseLayout>
+        <!-- 左エリアの内容 -->
+        <template #left>
+            <h1>Skills</h1>
+        </template>
+
+        <!-- 左エリアの内容 -->
+        <template #right>
+        </template>
+    </BaseLayout>
+</template>
