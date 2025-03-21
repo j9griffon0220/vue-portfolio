4ace1e7c5687bcf410fb14ccfee12a0b1cb2ad43
Ayako Nakayama <j9griffon0220@gmail.com>
2025-03-21 19:59:57 +0900

    index.jsで各componentsページのルーティングを設定

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from '../components/Home.vue'; // ホームコンポーネント
-import About from '../components/About.vue'; // アバウトコンポーネント
+// index.jsでルーティングを設定
+import { createRouter, createWebHistory } from 'vue-router'
 
+// 各ページコンポーネントを読み込む
+import Home from '@/views/Home.vue'
+import About from '@/views/About.vue'
+import Skills from '@/views/Skills.vue'
+import Works from '@/views/Works.vue'
+import Note from '@/views/Note.vue'
+
+// routes の component に importしたコンポーネントを指定
 const routes = [
   { path: '/', component: Home },
   { path: '/about', component: About },
-];
+  { path: '/skills', component: Skills },
+  { path: '/works', component: Works },
+  { path: '/note', component: Note },
+]
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
-});
+})
 
-export default router;
+export default router
