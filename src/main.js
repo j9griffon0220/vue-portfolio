b257bb49c7b89d976fa4376bdded7927f5eb9491
Ayako Nakayama <j9griffon0220@gmail.com>
2025-03-21 20:09:21 +0900

    main.jsに@vueuse/headの設定を追加

--- src/main.js
+++ src/main.js
@@ -1,10 +1,14 @@
+// Vueアプリケーションのエントリーポイント
+
 import { createApp } from "vue";
-import "./style.css";
-import App from "./App.vue";
-import router from './router'; // ルーターをインポート
+import { createHead } from "@vueuse/head";
+import "@/style.css";
+import App from "@/App.vue";
+import router from '@/router'; // ルーターをインポート
 
 // Vueアプリを作成して `app` に保存
 const app = createApp(App);
+const head = createHead()
 
 // GPT先生と相談のうえ追加 25.3.9
 // 開発環境でのみ Vue DevTools を有効にする（本番環境では無効）
@@ -13,6 +17,8 @@ if (import.meta.env.DEV === "development") {
 }
 
 app.use(router); // ルーターを使用
+
+app.use(head) // vueアプリにheadを追加
 
 // Vueアプリを画面にマウント
 app.mount("#app");
