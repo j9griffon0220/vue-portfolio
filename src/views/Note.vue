1564eb71dbd6aa07c7edf3e2de1e56b902251a9d
Ayako Nakayama <j9griffon0220@gmail.com>
2025-03-21 20:02:30 +0900

    Noteページのviewsファイルを作成

---
+++ src/views/Note.vue
@@ -0,0 +1,26 @@
+<script setup>
+import BaseLayout from '@/layouts/BaseLayout.vue'
+import { useHead } from '@vueuse/head'
+
+useHead({
+    title: 'Note | Ayako Nakayama portfolio',
+    meta: [
+        {name: 'description', content: '記事紹介ページです。'},
+        {property: 'og:title', content: 'Note | Ayako Nakayama portfolio'},
+        {property: 'og:url', content: 'https://your-portfolio.com/note'}
+    ]
+})
+</script>
+
+<template>
+    <BaseLayout>
+        <!-- 左エリアの内容 -->
+        <template #left>
+            <h1>Note</h1>
+        </template>
+
+        <!-- 左エリアの内容 -->
+        <template #right>
+        </template>
+    </BaseLayout>
+</template>
