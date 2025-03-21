2f06e654bc404c0a751f8bbbcdc8683b3b0c32a6
Ayako Nakayama <j9griffon0220@gmail.com>
2025-03-21 20:03:23 +0900

    Homeページのviewsファイルを作成

---
+++ src/views/Home.vue
@@ -0,0 +1,16 @@
+<script setup>
+import BaseLayout from '@/layouts/BaseLayout.vue'
+</script>
+
+<template>
+    <BaseLayout>
+        <!-- 左エリアの内容 -->
+        <template #left>
+            <h1 class="">Ayako Nakayama portfolio</h1>
+        </template>
+
+        <!-- 左エリアの内容 -->
+        <template #right>
+        </template>
+    </BaseLayout>
+</template>
