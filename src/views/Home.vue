91f10086b49d0f8204dd095baf1abba9e40079eb
Ayako Nakayama <j9griffon0220@gmail.com>
2025-04-08 20:22:12 +0900

    topページ、Home.vueファイルを作成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,27 @@
 <script setup>
-import BaseLayout from '@/layouts/BaseLayout.vue'
+//import BaseLayout from '@/layouts/BaseLayout.vue'
+import { ref } from 'vue'
+import { useHead } from '@vueuse/head'
+
+useHead({
+    title: 'Ayako Nakayama portfolio',
+    meta: [
+        {name: 'description', content: 'ポートフォリオトップページです。'},
+        {property: 'og:title', content: 'Ayako Nakayama portfolio'},
+        {property: 'og:url', content: 'https://your-portfolio.com'}
+    ]
+})
 </script>
 
 <template>
-    <BaseLayout>
+    <div>
         <!-- 左エリアの内容 -->
-        <template #left>
+        <template>
             <h1 class="">Ayako Nakayama portfolio</h1>
         </template>
 
         <!-- 左エリアの内容 -->
-        <template #right>
+        <template>
         </template>
-    </BaseLayout>
+    </div>
 </template>
