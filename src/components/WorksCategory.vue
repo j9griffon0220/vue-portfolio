8743bd915834bcf7b1730c454c41755b541de4fc
Ayako Nakayama <j9griffon0220@gmail.com>
2025-04-08 20:35:42 +0900

    Worksページの左側、categoryのコンポーネントを作成

---
+++ src/components/WorksCategory.vue
@@ -0,0 +1,27 @@
+<!-- Worksページ・左側のcomponents -->
+<script setup>
+import { ref } from 'vue';
+const categories = [
+    { label: 'Laravel制作サイト', value: 'laravelSite' },
+    { label: 'Vueポートフォリオ', value: 'vuePortfolio' }
+]
+
+// emitを定義（親のWorks.vueに通知するため）
+const emit = defineEmits(['update:selected-category'])
+
+const handleClick = (value) => {
+    emit('update:selected-category', value)
+}
+</script>
+
+<template>
+    <ul>
+        <li
+        v-for="category in categories"
+        :key="category.value"
+        @click="handleClick(category.value)"
+        :class="{ active: selectedCategory === category.value }">
+        {{ category.label }}
+        </li>
+    </ul>
+</template>
