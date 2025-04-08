55715520fe2f19749e775a8255c3e5ef6e1d9e08
Ayako Nakayama <j9griffon0220@gmail.com>
2025-04-08 20:34:21 +0900

    Skillsページの左側、categoryのコンポーネントを作成

---
+++ src/components/SkillsCategory.vue
@@ -0,0 +1,25 @@
+<!-- Skillsページ・左側のcomponents -->
+<script setup>
+import { ref } from 'vue'
+const categories = ['フロントエンド','バックエンド','環境構築・ツール','テスト・その他']
+
+// emitを定義（親のSkills.vueに通知するため）
+const emit = defineEmits(['update:selected-category'])
+
+// カテゴリーがクリックされたときに実行
+const handleCategorySelect = (category) => {
+    emit('update:selected-category', category) // ここがv-model対応部分
+}
+</script>
+
+<template>
+    <ul>
+        <li v-for="category in categories"
+        :key="category"
+        @click = "handleCategorySelect(category)">
+        {{ category }}
+        </li>
+    </ul>
+</template>
+
+<style scoped></style>
