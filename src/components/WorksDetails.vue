103d7cfc7397c6da26b137cd0a164a97b9213d42
Ayako Nakayama <j9griffon0220@gmail.com>
2025-04-19 16:22:58 +0900

    Works右側詳細ページ、Tabやカテゴリーごとの条件分岐を追加調整

--- src/components/WorksDetails.vue
+++ src/components/WorksDetails.vue
@@ -1,11 +1,16 @@
 <!-- Worksページ・右側のcomponents -->
 <script setup>
+import { ref } from 'vue'
+
 // 親から渡される props、definePropsで受け取る
 const props = defineProps({
     selectedCategory: {
         type: String,
         default: 'Laravel制作サイト', //デフォルト値を念のため設定
     },
+    selectedCategoryLabel: {
+        type: String,
+    }
 })
 
 const tabs = [
@@ -14,29 +19,37 @@ const tabs = [
     { label: 'Process', value: 'process' },
     { label: 'Result', value: 'result' }
 ]
-
 // デフォルトのタブを指定
 const selectedTab = ref('intro')
+
 </script>
 
 <template>
     <div>
-        <h2>{{ props.selectedCategory }}</h2>
+        <h2>{{ selectedCategoryLabel }}</h2>
 
         <!-- タブボタン -->
         <button
         v-for="tab in tabs"
         :key="tab.value"
-        @click="selectedTab.value = tab.value"
-        :class="{ active: selectedTab.value === tab.value }"
+        @click="selectedTab = tab.value"
+        :class="{ active: selectedTab === tab.value }"
         >
         {{ tab.label }}
         </button>
 
-        <!-- タブの内容 -->
-        <div v-show="selectedTab.value === 'intro'">Introの内容</div>
-        <div v-show="selectedTab.value === 'tech'">Techの内容</div>
-        <div v-show="selectedTab.value === 'process'">Processの内容</div>
-        <div v-show="selectedTab.value === 'result'">Resultの内容</div>
+        <!-- タブの内容・カテゴリーによって各タブ切り替え -->
+        <div v-if="props.selectedCategory === 'laravelSite'">
+            <div v-show="selectedTab === 'intro'">LaravelのIntroの内容</div>
+            <div v-show="selectedTab === 'tech'">LaravelのTechの内容</div>
+            <div v-show="selectedTab === 'process'">LaravelのProcessの内容</div>
+            <div v-show="selectedTab === 'result'">LaravelのResultの内容</div>
+        </div>
+        <div v-if="props.selectedCategory === 'vuePortfolio'">
+            <div v-show="selectedTab === 'intro'">VueポートフォリオのIntroの内容</div>
+            <div v-show="selectedTab === 'tech'">VueポートフォリオのTechの内容</div>
+            <div v-show="selectedTab === 'process'">VueポートフォリオのProcessの内容</div>
+            <div v-show="selectedTab === 'result'">VueポートフォリオのResultの内容</div>
+        </div>
     </div>
 </template>
