1803b7bbd1a708bf95ff4bf754b311e44168fdb7
Ayako Nakayama <j9griffon0220@gmail.com>
2025-05-11 15:30:18 +0900

    WorksページのタブTechをカットしてApproachに統合

--- src/components/works/LaravelTech.vue
+++
@@ -1,7 +0,0 @@
-<!--  Worksページ・右側components LaravelのTechタブ中身-->
-<script setup></script>
-
-<template>
-    <h3>概要</h3>
-    <p>これはlaravel技術です</p>
-</template>
--- src/components/works/VueTech.vue
+++
@@ -1,7 +0,0 @@
-<!--  Worksページ・右側components VueのTechタブ中身-->
-<script setup></script>
-
-<template>
-    <h3>概要</h3>
-    <p>これはvue技術です</p>
-</template>
