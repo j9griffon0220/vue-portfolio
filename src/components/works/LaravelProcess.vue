7bdcc2a99f80164bcbdc1220afd0e3ddcabbd39b
Ayako Nakayama <j9griffon0220@gmail.com>
2025-05-11 15:31:20 +0900

    WorksページのタブProcessをカットしてApproachに統合

--- src/components/works/LaravelProcess.vue
+++
@@ -1,7 +0,0 @@
-<!--  Worksページ・右側components LaravelのProcessタブ中身-->
-<script setup></script>
-
-<template>
-    <h3>概要</h3>
-    <p>これはlaravelProcessです</p>
-</template>
--- src/components/works/VueProcess.vue
+++
@@ -1,7 +0,0 @@
-<!--  Worksページ・右側components VueのProcessタブ中身-->
-<script setup></script>
-
-<template>
-    <h3>概要</h3>
-    <p>これはVueProcessです</p>
-</template>
