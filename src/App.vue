67ec7a60e642a2de3855fb7a8366d9b3338ee996
Ayako Nakayama <j9griffon0220@gmail.com>
2025-05-24 19:20:08 +0900

    App.vueを微調整（汗。ほとんど変化なし）

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
 <!-- ルーティング管理専用にする -->
 <script setup>
-import BaseLayout from "@/layouts/BaseLayout.vue"
+import BaseLayout from '@/layouts/BaseLayout.vue'
 </script>
 
 <template>
   <BaseLayout />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
