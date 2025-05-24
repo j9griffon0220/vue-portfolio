a62452f4dfd2f0721afc4fe6a4b9f68218464c2e
Ayako Nakayama <j9griffon0220@gmail.com>
2025-05-24 19:35:06 +0900

    Prettierによるコード反映・修正

--- src/components/SkillsCategory.vue
+++ src/components/SkillsCategory.vue
@@ -1,25 +1,33 @@
 <!-- Skillsページ・左側のcomponents -->
 <script setup>
 import { ref } from 'vue'
-const categories = ['フロントエンド','バックエンド','環境構築・ツール','テスト・その他']
+const categories = [
+  'フロントエンド',
+  'バックエンド',
+  '環境構築・ツール',
+  'テスト・その他',
+]
 
 // emitを定義（親のSkills.vueに通知するため）
 const emit = defineEmits(['update:selected-category'])
 
 // カテゴリーがクリックされたときに実行
 const handleCategorySelect = (category) => {
-    emit('update:selected-category', category) // ここがv-model対応部分
+  emit('update:selected-category', category) // ここがv-model対応部分
 }
 </script>
 
 <template>
-    <ul>
-        <li v-for="category in categories"
-        :key="category"
-        @click = "handleCategorySelect(category)">
-        {{ category }}
-        </li>
-    </ul>
+  <ul>
+    <li
+      class="font-ibm font-normal"
+      v-for="category in categories"
+      :key="category"
+      @click="handleCategorySelect(category)"
+    >
+      {{ category }}
+    </li>
+  </ul>
 </template>
 
 <style scoped></style>
