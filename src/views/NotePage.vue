6520cbc62aae1fb36b702bac0b8bea0b4956f363
Ayako Nakayama <j9griffon0220@gmail.com>
2025-05-24 19:29:06 +0900

    ESLintの指摘でコンポーネント名を調整

---
+++ src/views/NotePage.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { useHead } from '@vueuse/head'
+
+useHead({
+  title: 'Note | Ayako Nakayama portfolio',
+  meta: [
+    { name: 'description', content: '記事紹介ページです。' },
+    { property: 'og:title', content: 'Note | Ayako Nakayama portfolio' },
+    { property: 'og:url', content: 'https://your-portfolio.com/note' },
+  ],
+})
+</script>
+
+<template>
+  <div>
+    <!-- 左エリアの内容 -->
+    <templat>
+      <h1>Note</h1>
+    </templat>
+
+    <!-- 左エリアの内容 -->
+    <template> </template>
+  </div>
+</template>
