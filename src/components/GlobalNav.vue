eb3cce46278fc4abc866f3a02bf852b12aa9fff7
Ayako Nakayama <j9griffon0220@gmail.com>
2025-05-24 19:37:06 +0900

    tailwindによるスタイリング追加、Prettierによるコード反映・修正

--- src/components/GlobalNav.vue
+++ src/components/GlobalNav.vue
@@ -1,24 +1,29 @@
 <script setup>
 const navItems = [
-    { text: 'Home', link: '/' },
-    { text: 'About', link: '/about' },
-    { text: 'Skills', link: '/Skills' },
-    { text: 'Works', link: '/works' },
-    { text:'Note', link: '/note' }
+  { text: 'Home', link: '/' },
+  { text: 'About', link: '/about' },
+  { text: 'Skills', link: '/Skills' },
+  { text: 'Works', link: '/works' },
+  { text: 'Note', link: '/note' },
 ]
 </script>
 
 <template>
-    <nav>
-        <ul>
-            <li v-for="item in navItems" :key="item.text">
-                <router-link :to="item.link">
-                    {{ item.text }}
-                </router-link>
-            </li>
-        </ul>
-    </nav>
+  <nav>
+    <ul
+      class="font-ibm text-charcoal-gray flex list-none justify-center font-normal"
+    >
+      <li
+        v-for="item in navItems"
+        :key="item.text"
+        class="text-[calc(0.9rem+0.25vw)] leading-[1.5]"
+      >
+        <router-link :to="item.link">
+          {{ item.text }}
+        </router-link>
+      </li>
+    </ul>
+  </nav>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
