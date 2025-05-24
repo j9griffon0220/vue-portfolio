28846ccd1a66df3d3788cdd4b8ebd8817d54feef
Ayako Nakayama <j9griffon0220@gmail.com>
2025-05-24 19:27:36 +0900

    ESLintの指摘でコンポーネント名を調整

--- src/components/Footer.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-</script>
-
-<template>
-  <footer>&copy; Ayako Nakayama 2025</footer>
-</template>
-
-<style scoped></style>
---
+++ src/components/SiteFooter.vue
@@ -0,0 +1,9 @@
+<script setup></script>
+
+<template>
+  <footer class="font-ibm text-soft-gray text-center font-light">
+    &copy; Ayako Nakayama 2025
+  </footer>
+</template>
+
+<style scoped></style>
