ce3db0ac444426ab1b8cda363a13cc50739c9995
Ayako Nakayama <j9griffon0220@gmail.com>
2025-05-24 19:39:32 +0900

    tailwindによるスタイリング追加、Prettierによるコード反映・修正

--- src/components/WorksCategory.vue
+++ src/components/WorksCategory.vue
@@ -7,24 +7,26 @@ const emit = defineEmits(['update:selected-category'])
 
 // 親から渡される props、definePropsで受け取る
 const props = defineProps({
-    categories: Array,
-    selectedCategory: String
+  categories: Array,
+  selectedCategory: String,
 })
 
 const handleClick = (value) => {
-    // 親にイベントを通知
-    emit('update:selected-category', value)
+  // 親にイベントを通知
+  emit('update:selected-category', value)
 }
 </script>
 
 <template>
-    <ul>
-        <li
-        v-for="category in categories"
-        :key="category.value"
-        :class="{ active: category.value === selectedCategory}"
-        @click="handleClick(category.value)">
-        {{ category.label }}
-        </li>
-    </ul>
+  <ul>
+    <li
+      class="font-ibm text-charcoal-gray text-[calc(0.9rem+0.25vw)] leading-[1.5] font-normal"
+      v-for="category in categories"
+      :key="category.value"
+      :class="{ active: category.value === selectedCategory }"
+      @click="handleClick(category.value)"
+    >
+      {{ category.label }}
+    </li>
+  </ul>
 </template>
