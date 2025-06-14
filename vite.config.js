53cfb07b496b8fb55b8369c5448ca7619a140cc0
Ayako Nakayama <j9griffon0220@gmail.com>
2025-06-14 13:39:43 +0900

    vite.config.jsにvite-svg-loaderをプラグインとして登録

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,13 @@ import vue from '@vitejs/plugin-vue'
 // TailwindのViteプラグインをインポート
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import svgLoader from 'vite-svg-loader'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    svgLoader(), // ← vite-svg-loaderを追加
     tailwindcss(), // ← Tailwindのプラグインを追加
   ],
   resolve: {
