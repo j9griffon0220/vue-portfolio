42131ac8974ada6b3eb4066685816e248c0f2b66
Ayako Nakayama <j9griffon0220@gmail.com>
2025-06-14 13:40:47 +0900

    vue のテンプレートファイル（別名保存作成用）を作成

---
+++ src/example.vue
@@ -0,0 +1,13 @@
+<!-- vue の雛形ファイル -->
+<script setup>
+// importや状態の定義
+</script>
+
+<template>
+  <div class="example">
+    <!-- コンテンツ -->
+  </div>
+</template>
+
+<!-- 必要ならスタイル -->
+<style scoped></style>
