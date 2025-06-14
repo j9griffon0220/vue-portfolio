ac7333d69c92392125349575ff1a357afdee7987
Ayako Nakayama <j9griffon0220@gmail.com>
2025-06-14 14:05:23 +0900

    Worksの詳細laravelタブにtailwindのスタイリングを追加・調整中

--- src/components/works/LaravelIntro.vue
+++ src/components/works/LaravelIntro.vue
@@ -4,20 +4,25 @@
 <template>
   <div class="">
     <h3
-      class="font-ibm text-charcoal-gray text-[calc(1.27rem+0.27vw)] leading-[1.4] font-medium"
+      class="font-ibm text-charcoal-gray mb-[clamp(1.56rem,1.56rem+0.25vw,1.95rem)] text-[calc(1.27rem+0.27vw)] leading-[1.4] font-medium"
     >
       サイト概要
     </h3>
     <!-- スクリーンショット画像 imgタグ -->
-    <p
-      class="font-ibm text-soft-gray text-[calc(0.9rem+0.25vw)] leading-[1.5] font-normal whitespace-normal"
+    <div
+      class="font-ibm text-soft-gray space-y-[clamp(1.25rem,1.25rem+0.20vw,1.56rem)] text-[calc(0.9rem+0.25vw)] leading-[1.5] font-normal whitespace-normal"
     >
-      カウンセラーの知人向けに、お問い合わせフォーム付きの紹介用サイトをLaravelで構築しました。
-      <br />
-      問い合わせを受けられるようフォームを設置し、送信内容がクライアントの Gmail
-      アカウントへ直接届く仕組みを導入。シンプルで使いやすい問い合わせフローを実現しています。
-      <br />またフレームワークの構造理解を深めることも目的とし、
-      Laravelのルーティング、メール送信処理、環境変数の設定などを実践的に経験しました。
-    </p>
+      <p>
+        カウンセラーの知人向けに、お問い合わせフォーム付きの紹介用サイトをLaravelで構築しました。
+      </p>
+      <p>
+        問い合わせを受けられるようフォームを設置し、送信内容がクライアントの
+        Gmailアカウントへ直接届く仕組みを導入。シンプルで使いやすい問い合わせフローを実現しています。
+      </p>
+      <p>
+        またフレームワークの構造理解を深めることも目的とし、
+        Laravelのルーティング、メール送信処理、環境変数の設定などを実践的に経験しました。
+      </p>
+    </div>
   </div>
 </template>
