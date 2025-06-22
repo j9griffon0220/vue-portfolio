e0d6b4e3066ee37217edbc2d8cfbb532b3f70906
Ayako Nakayama <j9griffon0220@gmail.com>
2025-06-22 16:49:28 +0900

    アイコンを一元管理するためのコンポーネントとしてIcon.vueを作成（@ではなく相対パスでsvgを読み込む）

---
+++ src/components/Icon.vue
@@ -0,0 +1,24 @@
+<!-- アイコンの管理を一元化・再利用性を高める -->
+<script setup>
+// importや状態の定義
+import { defineAsyncComponent, computed, defineProps } from 'vue'
+
+// 'name'を受け取り対応するSVGを動的に取り込む
+const props = defineProps({
+  name: { type: String, required: true }, // 'vue' → 'vue.svg'を読み込む
+})
+
+// バッククォート ＆ 相対パスで動的にSVGを読み込む
+const iconComponent = defineAsyncComponent(
+  () => import(`../assets/icons/${props.name}.svg`),
+)
+
+// エラーハンドリングの記述まだ
+</script>
+
+<template>
+  <component :is="iconComponent" />
+</template>
+
+<!-- 必要ならスタイル -->
+<style scoped></style>
