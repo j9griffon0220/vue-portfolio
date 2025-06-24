758fd60a86fb779aa672bee09b8a11892997ebf2
Ayako Nakayama <j9griffon0220@gmail.com>
2025-06-24 22:04:53 +0900

    cursor生成によるヒーローアニメーション、ランダムロゴSVG動きのパーツ

---
+++ src/components/animation/DeviconCloud.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Icon from '@/components/Icon.vue'
+defineProps({
+  icons: Array,
+  skillIcons: Array,
+  step: Number
+})
+</script>
+
+<template>
+  <div v-for="(icon, index) in icons" :key="index"
+    class="absolute w-12 h-12 transition-all duration-1000 ease-in-out"
+    :style="{
+      left: `${icon.x}px`,
+      top: `${icon.y}px`,
+      opacity: icon.opacity,
+      transform: `scale(${icon.scale})`,
+      zIndex: step === 1 ? 20 : 5
+    }"
+  >
+    <Icon :name="skillIcons[index]" class="w-full h-full" />
+  </div>
+</template>
