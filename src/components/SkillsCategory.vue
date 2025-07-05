cb9289dd0f80927309e0a5fcf1684393baf7b79e
Ayako Nakayama <j9griffon0220@gmail.com>
2025-07-05 14:43:42 +0900

    skillsページの左側、tailwindでのスタイリングのために定数追加、tailwind調整

--- src/components/SkillsCategory.vue
+++ src/components/SkillsCategory.vue
@@ -8,6 +8,14 @@ const categories = [
   'テスト・その他',
 ]
 
+// 親から渡される props、definePropsで受け取る
+const props = defineProps({
+  selectedCategory: {
+    type: String,
+    default: 'フロントエンド', //デフォルト値を念のため設定
+  },
+})
+
 // emitを定義（親のSkills.vueに通知するため）
 const emit = defineEmits(['update:selected-category'])
 
@@ -18,12 +26,17 @@ const handleCategorySelect = (category) => {
 </script>
 
 <template>
-  <ul>
+  <ul class="">
     <li
-      class="font-ibm text-charcoal-gray mb-[clamp(1.95rem,1.95rem+0.30vw,2.44rem)] text-[calc(0.9rem+0.25vw)] leading-[1.5] font-normal"
       v-for="category in categories"
       :key="category"
       @click="handleCategorySelect(category)"
+      :class="[
+        'font-ibm mb-[clamp(1.95rem,1.95rem+0.30vw,2.44rem)] max-w-8/10 px-4 py-2 text-[calc(0.85rem+0.2vw)] leading-[1.5] font-medium transition-colors duration-200 ease-in-out',
+        category === selectedCategory
+          ? 'text-charcoal-gray hover:bg-charcoal-gray/5 active:bg-charcoal-gray/50 active:text-gradation-white'
+          : 'text-charcoal-gray/60 hover:bg-charcoal-gray/5',
+      ]"
     >
       {{ category }}
     </li>
