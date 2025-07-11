3611bc9c8fe6160689ef7d5da57f9838ea453a1d
Ayako Nakayama <j9griffon0220@gmail.com>
2025-07-11 19:50:03 +0900

    Skillsの左側Categoryを調整、activeの設定を追加

--- src/components/SkillsCategory.vue
+++ src/components/SkillsCategory.vue
@@ -32,9 +32,9 @@ const handleCategorySelect = (category) => {
       :key="category"
       @click="handleCategorySelect(category)"
       :class="[
-        'font-ibm mb-[clamp(1.95rem,1.95rem+0.30vw,2.44rem)] max-w-8/10 px-4 py-2 text-[calc(0.85rem+0.2vw)] leading-[1.5] font-medium transition-colors duration-200 ease-in-out',
+        'font-ibm mb-[clamp(1.95rem,1.95rem+0.30vw,2.44rem)] max-w-8/10 px-4 py-2 text-[calc(0.85rem+0.2vw)] leading-[1.5] font-medium transition-colors duration-150 ease-in-out',
         category === selectedCategory
-          ? 'text-charcoal-gray hover:bg-charcoal-gray/5 active:bg-charcoal-gray/50 active:text-gradation-white'
+          ? 'active:bg-charcoal-gray/50 active:text-gradation-white text-charcoal-gray hover:bg-charcoal-gray/5 active:scale-[.98]'
           : 'text-charcoal-gray/60 hover:bg-charcoal-gray/5',
       ]"
     >
