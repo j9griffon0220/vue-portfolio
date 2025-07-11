58741a71d0506b19acd2d6f5f86ea0ef40520fb9
Ayako Nakayama <j9griffon0220@gmail.com>
2025-07-11 19:46:16 +0900

    Gナビにactiveの設定を追加

--- src/components/GlobalNav.vue
+++ src/components/GlobalNav.vue
@@ -28,7 +28,7 @@ const handleSelect = (item) => {
         :class="[
           'font-ibm relative px-4 py-2 text-[calc(0.85rem+0.2vw)] leading-[1.5] font-medium transition-colors duration-200 ease-in-out',
           selectedItem === item.text
-            ? 'text-charcoal-gray hover:bg-charcoal-gray/5 active:bg-charcoal-gray/50 active:text-gradation-white'
+            ? 'active:bg-charcoal-gray/50 active:text-gradation-white text-charcoal-gray hover:bg-charcoal-gray/5 active:scale-[.98]'
             : 'text-charcoal-gray/60 hover:bg-charcoal-gray/5',
         ]"
         role="navigation"
