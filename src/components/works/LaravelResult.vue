90f4652c716cbba12e7cc4204a3859253e039184
Ayako Nakayama <j9griffon0220@gmail.com>
2025-07-11 20:15:39 +0900

    Worksの右側、laravelResultタブの文言を追加

--- src/components/works/LaravelResult.vue
+++ src/components/works/LaravelResult.vue
@@ -2,8 +2,34 @@
 <script setup></script>
 
 <template>
-  <h3 class="font-ibm text-charcoal-gray font-medium">成果・振り返り</h3>
+  <h3
+    class="font-ibm text-charcoal-gray text-[calc(1.2rem+0.27vw)] leading-[1.4] font-medium"
+  >
+    成果・振り返り
+  </h3>
 
-  <h4 class="font-ibm text-charcoal-gray font-medium">成果</h4>
-  <p class="font-ibm text-soft-gray font-normal">こんな成果が〜</p>
+  <h4
+    class="font-ibm text-charcoal-gray text-[calc(1.05rem+0.25vw)] leading-[1.5] font-medium"
+  >
+    成果
+  </h4>
+  <p
+    class="font-ibm text-soft-gray text-[calc(0.9rem+0.25vw)] leading-[1.5] font-normal"
+  >
+    公開後、お問い合わせフォームにチェックボックス追加の要望があり、バリデーション処理や画面表示の調整を行いました。
+    <br />併せてテストコードを追加・実行し、変更後も安定した挙動を確認しました。
+    <br />GitHub上でプルリクエストを通じて修正を反映し、運用フェーズにも対応できる構成を意識しました。
+  </p>
+
+  <h4
+    class="font-ibm text-charcoal-gray text-[calc(1.05rem+0.25vw)] leading-[1.5] font-medium"
+  >
+    振り返り
+  </h4>
+  <p
+    class="font-ibm text-soft-gray text-[calc(0.9rem+0.25vw)] leading-[1.5] font-normal"
+  >
+    今回の制作を通じて、「運用フェーズでの対応」まで含めた開発フローの一端を経験することができました。
+    <br />初期段階ではフォームの追加・変更は想定していませんでしたが、運用後の変更に備える視点やテストの重要性を改めて実感する機会となりました。
+  </p>
 </template>
