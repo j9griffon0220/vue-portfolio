7e32a04b4e130538d876eb777e039fc96911f9e1
Ayako Nakayama <j9griffon0220@gmail.com>
2025-07-11 20:02:01 +0900

    BaseLayout、トップページの時のみブラウザの高さに収めるように調整

--- src/layouts/BaseLayout.vue
+++ src/layouts/BaseLayout.vue
@@ -5,18 +5,26 @@ if (import.meta.env.VITE_DEBUG === 'true') {
 }
 import Header from '@/components/SiteHeader.vue'
 import Footer from '@/components/SiteFooter.vue'
+import { useRoute } from 'vue-router'
+
+// 現在のルート情報を取得
+const route = useRoute()
 </script>
 
 <template>
-  <div class="bg-gradient-test min-h-screen">
+  <div class="bg-gradient-test">
+    <!-- 条件分岐：トップページ（Home.vue）の時だけブラウザの高さにヘッダーフッター含めピッタリ収める -->
     <div
-      class="mx-auto max-w-[90vw] pt-[clamp(1.56rem,1.56rem+0.25vw,1.95rem)]"
+      :class="[
+        'mx-auto max-w-[90vw] pt-[clamp(1.56rem,1.56rem+0.25vw,1.95rem)] pb-[clamp(1.56rem,1.56rem+0.25vw,1.95rem)]',
+        route.path === '/' ? 'flex h-screen flex-col' : '',
+      ]"
     >
       <!-- 高さ指定なし -->
       <Header />
 
       <!-- メインコンテンツ（各ページのviewsファイルが入る） -->
-      <main class="mx-auto max-w-[85vw] flex-1 overflow-hidden">
+      <main class="flex-1 overflow-hidden">
         <router-view />
       </main>
       <!-- 高さ指定なし -->
