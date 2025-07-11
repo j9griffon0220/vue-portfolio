c5d551dd697a9af316f5647231ec829bae9d6b04
Ayako Nakayama <j9griffon0220@gmail.com>
2025-07-11 20:17:19 +0900

    Worksの右側、VueResultタブのスタイリング調整中

--- src/components/works/VueResult.vue
+++ src/components/works/VueResult.vue
@@ -2,6 +2,15 @@
 <script setup></script>
 
 <template>
-  <h3 class="font-ibm font-medium">概要</h3>
+  <h3
+    class="font-ibm text-charcoal-gray text-[calc(1.2rem+0.27vw)] leading-[1.4] font-medium"
+  >
+    成果・振り返り
+  </h3>
+  <h4
+    class="font-ibm text-charcoal-gray text-[calc(1.05rem+0.25vw)] leading-[1.5] font-medium"
+  >
+    成果
+  </h4>
   <p class="font-ibm font-normal">これはVueResultです</p>
 </template>
