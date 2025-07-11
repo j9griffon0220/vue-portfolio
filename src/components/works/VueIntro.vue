87e21983b1571750ac9e6d3b069b4706b679b22a
Ayako Nakayama <j9griffon0220@gmail.com>
2025-07-11 20:16:33 +0900

    Worksの右側、VueIntroタブのスタイリング調整中

--- src/components/works/VueIntro.vue
+++ src/components/works/VueIntro.vue
@@ -2,7 +2,14 @@
 <script setup></script>
 
 <template>
-  <h3 class="font-ibm font-medium">サイト概要</h3>
+  <h3
+    class="font-ibm text-charcoal-gray mb-[clamp(1.56rem,1.56rem+0.25vw,1.95rem)] text-[calc(1.27rem+0.27vw)] leading-[1.4] font-medium"
+  >
+    サイト概要
+  </h3>
   <!-- スクリーンショット画像 imgタグ -->
+  <div
+    class="font-ibm text-soft-gray space-y-[clamp(1.25rem,1.25rem+0.20vw,1.56rem)] text-[calc(0.9rem+0.25vw)] leading-[1.5] font-normal whitespace-normal"
+  ></div>
   <p class="font-ibm font-normal">これはvue概要です</p>
 </template>
