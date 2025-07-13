534df6b038104dc43e9fe52044be74c45cca8bc5
Ayako Nakayama <j9griffon0220@gmail.com>
2025-07-13 14:20:49 +0900

    WorksぺーじのLaravelのResultタブ、上下の余白やスタイルを調整

--- src/components/works/LaravelResult.vue
+++ src/components/works/LaravelResult.vue
@@ -3,33 +3,43 @@
 
 <template>
   <h3
-    class="font-ibm text-charcoal-gray text-[calc(1.2rem+0.27vw)] leading-[1.4] font-medium"
+    class="font-ibm text-charcoal-gray mb-[clamp(1.56rem,1.56rem+0.25vw,1.95rem)] text-[calc(1.2rem+0.27vw)] leading-[1.4] font-medium"
   >
     成果・振り返り
   </h3>
 
   <h4
-    class="font-ibm text-charcoal-gray text-[calc(1.05rem+0.25vw)] leading-[1.5] font-medium"
+    class="font-ibm text-charcoal-gray mb-[clamp(1rem,1rem+0vw,1rem)] text-[calc(1.05rem+0.25vw)] leading-[1.5] font-medium"
   >
     成果
   </h4>
-  <p
-    class="font-ibm text-soft-gray text-[calc(0.9rem+0.25vw)] leading-[1.5] font-normal"
+  <div
+    class="font-ibm text-soft-gray mb-[clamp(1.56rem,1.56rem+0.25vw,1.95rem)] space-y-[clamp(1.25rem,1.25rem+0.20vw,1.56rem)] text-[calc(0.9rem+0.25vw)] leading-[1.5] font-normal whitespace-normal"
   >
-    公開後、お問い合わせフォームにチェックボックス追加の要望があり、バリデーション処理や画面表示の調整を行いました。
-    <br />併せてテストコードを追加・実行し、変更後も安定した挙動を確認しました。
-    <br />GitHub上でプルリクエストを通じて修正を反映し、運用フェーズにも対応できる構成を意識しました。
-  </p>
+    <p>
+      公開後、お問い合わせフォームにチェックボックス追加の要望があり、バリデーション処理や画面表示の調整を行いました。
+    </p>
+    <p>
+      併せてテストコードを追加・実行し、変更後も安定した挙動を確認しました。
+    </p>
+    <p>
+      GitHub上でプルリクエストを通じて修正を反映し、運用フェーズにも対応できる構成を意識しました。
+    </p>
+  </div>
 
   <h4
-    class="font-ibm text-charcoal-gray text-[calc(1.05rem+0.25vw)] leading-[1.5] font-medium"
+    class="font-ibm text-charcoal-gray mb-[clamp(1rem,1rem+0vw,1rem)] text-[calc(1.05rem+0.25vw)] leading-[1.5] font-medium"
   >
     振り返り
   </h4>
-  <p
-    class="font-ibm text-soft-gray text-[calc(0.9rem+0.25vw)] leading-[1.5] font-normal"
+  <div
+    class="font-ibm text-soft-gray mb-[clamp(1.56rem,1.56rem+0.25vw,1.95rem)] space-y-[clamp(1.25rem,1.25rem+0.20vw,1.56rem)] text-[calc(0.9rem+0.25vw)] leading-[1.5] font-normal whitespace-normal"
   >
-    今回の制作を通じて、「運用フェーズでの対応」まで含めた開発フローの一端を経験することができました。
-    <br />初期段階ではフォームの追加・変更は想定していませんでしたが、運用後の変更に備える視点やテストの重要性を改めて実感する機会となりました。
-  </p>
+    <p>
+      今回の制作を通じて、「運用フェーズでの対応」まで含めた開発フローの一端を経験することができました。
+    </p>
+    <p>
+      初期段階ではフォームの追加・変更は想定していませんでしたが、運用後の変更に備える視点やテストの重要性を改めて実感する機会となりました。
+    </p>
+  </div>
 </template>
