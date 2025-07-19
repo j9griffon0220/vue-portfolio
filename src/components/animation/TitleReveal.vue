fbf5a7149cf3c6015086160edc99b7ac4ea28a53
Ayako Nakayama <j9griffon0220@gmail.com>
2025-07-19 11:56:15 +0900

    HeroAnimaのTitleRecealファイル、スタイリング調整中

--- src/components/animation/TitleReveal.vue
+++ src/components/animation/TitleReveal.vue
@@ -7,16 +7,18 @@ defineProps({ show: Boolean })
 
 <template>
   <transition name="fade">
-    <div
-      v-if="show"
-      class="absolute inset-0 z-40 flex items-center justify-center"
-    >
-      <!-- h1にtranslate-y-[-4rem]を入れてとりあえず見た目の位置を調整保留 一旦削除7.8-->
-      <h1
-        class="font-ibm text-charcoal-gray animate-fade-in text-center text-[calc(1.8rem+0.3vw)] leading-[1.2] font-semibold"
+    <div class="h-full">
+      <div
+        v-if="show"
+        class="absolute inset-0 z-40 flex items-center justify-center"
       >
-        Ayako Nakayama portfolio
-      </h1>
+        <!-- h1にtranslate-y-[-4rem]を入れてとりあえず見た目の位置を調整保留 一旦削除7.8-->
+        <h1
+          class="font-ibm text-charcoal-gray animate-fade-in text-center text-[calc(1.8rem+0.3vw)] leading-[1.2] font-semibold"
+        >
+          Ayako Nakayama portfolio
+        </h1>
+      </div>
     </div>
   </transition>
 </template>
