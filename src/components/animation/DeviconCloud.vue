2af4e3e9b61e0d88cc3895a8a57a2a306204c062
Ayako Nakayama <j9griffon0220@gmail.com>
2025-07-19 11:56:43 +0900

    HeroAnimaのDeviconファイル、スタイリング調整中

--- src/components/animation/DeviconCloud.vue
+++ src/components/animation/DeviconCloud.vue
@@ -11,21 +11,23 @@ defineProps({
 
 <!-- アイコンサイズはSkillsDetailsの時と同じサイズ感 -->
 <template>
-  <div
-    v-for="(icon, index) in icons"
-    :key="index"
-    class="absolute flex items-center justify-center transition-all duration-1000 ease-in-out"
-    :style="{
-      left: `${icon.x}px`,
-      top: `${icon.y}px`,
-      opacity: icon.opacity,
-      transform: `translate(-50%, -50%) scale(${icon.scale})`,
-      zIndex: step === 1 ? 20 : 5,
-      width: 'calc(1.8rem + 0.3vw)',
-      height: 'calc(1.8rem + 0.3vw)',
-    }"
-  >
-    <Icon :name="skillIcons[index]" class="text-accent-gray h-full w-full" />
+  <div class="h-full">
+    <div
+      v-for="(icon, index) in icons"
+      :key="index"
+      class="absolute flex items-center justify-center transition-all duration-1000 ease-in-out"
+      :style="{
+        left: `${icon.x}px`,
+        top: `${icon.y}px`,
+        opacity: icon.opacity,
+        transform: `translate(-50%, -50%) scale(${icon.scale})`,
+        zIndex: step === 1 ? 20 : 5,
+        width: 'calc(1.8rem + 0.3vw)',
+        height: 'calc(1.8rem + 0.3vw)',
+      }"
+    >
+      <Icon :name="skillIcons[index]" class="text-accent-gray h-full w-full" />
+    </div>
   </div>
 </template>
 
