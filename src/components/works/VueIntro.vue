7f47d268d9a51b5ba7f10f6c0e68145aa01928c0
Ayako Nakayama <j9griffon0220@gmail.com>
2025-07-19 11:43:35 +0900

    WorksページのVueIntroタブ、文言追加調整

--- src/components/works/VueIntro.vue
+++ src/components/works/VueIntro.vue
@@ -5,11 +5,12 @@
   <h3
     class="font-ibm text-charcoal-gray mb-[clamp(1.56rem,1.56rem+0.25vw,1.95rem)] text-[calc(1.27rem+0.27vw)] leading-[1.4] font-medium"
   >
-    サイト概要
+    Vueポートフォリオ概要
   </h3>
   <!-- スクリーンショット画像 imgタグ -->
   <div
     class="font-ibm text-soft-gray space-y-[clamp(1.25rem,1.25rem+0.20vw,1.56rem)] text-[calc(0.9rem+0.25vw)] leading-[1.5] font-normal whitespace-normal"
-  ></div>
-  <p class="font-ibm font-normal">これはvue概要です</p>
+  >
+    <p>ポートフォリオサイトを、Vue 3 + Vite + Tailwind CSS で構築しました。</p>
+  </div>
 </template>
