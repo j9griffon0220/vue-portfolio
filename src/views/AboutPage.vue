f0467430a4df5486199bd81c61cb882b620f8196
Ayako Nakayama <j9griffon0220@gmail.com>
2025-07-19 11:32:18 +0900

    AboutページにOGPの記述追加、本文の文言調整

--- src/views/AboutPage.vue
+++ src/views/AboutPage.vue
@@ -6,7 +6,9 @@ useHead({
   meta: [
     { name: 'description', content: '自己紹介ページです。' },
     { property: 'og:title', content: 'About | Ayako Nakayama portfolio' },
-    { property: 'og:url', content: 'https://your-portfolio.com/about' },
+    { property: 'og:description', content: '自己紹介ページです。' },
+    // og:urlまだ
+    { property: 'og:url', content: 'https://00000/about' },
   ],
 })
 </script>
@@ -23,12 +25,12 @@ useHead({
     <h2
       class="font-ibm text-charcoal-gray mb-[clamp(1.95rem,1.95rem+0.30vw,2.44rem)] text-[calc(1.51rem+0.29vw)] leading-[1.3] font-medium"
     >
-      h2調整中
+      自己紹介
     </h2>
     <h3
       class="font-ibm text-charcoal-gray mb-[clamp(1.56rem,1.56rem+0.25vw,1.95rem)] text-[calc(1.2rem+0.27vw)] leading-[1.4] font-medium"
     >
-      h3調整中
+      スキルと学びの履歴
     </h3>
     <div
       class="font-ibm text-soft-gray mb-[clamp(1.56rem,1.56rem+0.25vw,1.95rem)] space-y-[clamp(1.25rem,1.25rem+0.20vw,1.56rem)] text-[calc(0.9rem+0.25vw)] leading-[1.5] font-normal whitespace-normal"
@@ -41,7 +43,7 @@ useHead({
         宣伝会議主催「編集・ライター養成講座」修了（0000年） <br />-
         現職・エディターの仕事をしていく上で必要と考え受講
       </p>
-      <p>※調整中。今後も必要に応じて更新予定。</p>
+      <p>※現在、Aboutページの記述を調整中です。随時更新予定です。</p>
     </div>
     <!-- 左エリアの内容 -->
   </div>
