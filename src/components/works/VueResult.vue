892113f47df5034c3fd20797a9aeac1e6b722bb3
Ayako Nakayama <j9griffon0220@gmail.com>
2025-07-19 11:43:04 +0900

    WorksページのVueResultタブ、文言追加調整

--- src/components/works/VueResult.vue
+++ src/components/works/VueResult.vue
@@ -3,14 +3,36 @@
 
 <template>
   <h3
-    class="font-ibm text-charcoal-gray text-[calc(1.2rem+0.27vw)] leading-[1.4] font-medium"
+    class="font-ibm text-charcoal-gray mb-[clamp(1.56rem,1.56rem+0.25vw,1.95rem)] text-[calc(1.2rem+0.27vw)] leading-[1.4] font-medium"
   >
     成果・振り返り
   </h3>
+
   <h4
-    class="font-ibm text-charcoal-gray text-[calc(1.05rem+0.25vw)] leading-[1.5] font-medium"
+    class="font-ibm text-charcoal-gray mb-[clamp(1rem,1rem+0vw,1rem)] text-[calc(1.05rem+0.25vw)] leading-[1.5] font-medium"
   >
     成果
   </h4>
-  <p class="font-ibm font-normal">これはVueResultです</p>
+  <div
+    class="font-ibm text-soft-gray mb-[clamp(1.56rem,1.56rem+0.25vw,1.95rem)] space-y-[clamp(1.25rem,1.25rem+0.20vw,1.56rem)] text-[calc(0.9rem+0.25vw)] leading-[1.5] font-normal whitespace-normal"
+  >
+    <p>現在、Vueポートフォリオ Resultの記述を調整中です。随時更新予定です。</p>
+  </div>
+
+  <h4
+    class="font-ibm text-charcoal-gray mb-[clamp(1rem,1rem+0vw,1rem)] text-[calc(1.05rem+0.25vw)] leading-[1.5] font-medium"
+  >
+    振り返り
+  </h4>
+  <div
+    class="font-ibm text-soft-gray mb-[clamp(1.56rem,1.56rem+0.25vw,1.95rem)] space-y-[clamp(1.25rem,1.25rem+0.20vw,1.56rem)] text-[calc(0.9rem+0.25vw)] leading-[1.5] font-normal whitespace-normal"
+  >
+    <p>
+      Vue を扱う中で、Laravel での同期的な開発との違いを実感しました。Vue
+      では非同期処理が基本となり、状態管理や描画タイミングへの理解がより求められると感じています。
+    </p>
+    <p>
+      Vueではコンポーネント単位での設計が細かく、構成の粒度や再利用性を意識する場面が多いため、より「設計力」が問われると感じました。
+    </p>
+  </div>
 </template>
