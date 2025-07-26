92aa6b84f650cdd8d62882ee31cc7c1866b15106
Ayako Nakayama <j9griffon0220@gmail.com>
2025-07-26 16:04:34 +0900

    HeroIntroにて、HeroAnimationにh1座標を取得して集めるための設定追加

--- src/components/animation/HeroIntro.vue
+++ src/components/animation/HeroIntro.vue
@@ -3,6 +3,15 @@
 <script setup>
 // importや状態の定義
 defineProps({ show: Boolean })
+import { ref } from 'vue'
+
+// h1のDOMを保持するためのref
+const introTitle = ref(null)
+
+// 親の HeroAnimation からアクセスできるように公開
+defineExpose({
+  introTitle,
+})
 </script>
 
 <template>
@@ -12,6 +21,7 @@ defineProps({ show: Boolean })
       class="absolute inset-0 z-10 flex items-center justify-center"
     >
       <h1
+        ref="introTitle"
         class="font-ibm text-charcoal-gray text-center text-[calc(1.8rem+0.3vw)] leading-[1.2] font-semibold"
       >
         edit
