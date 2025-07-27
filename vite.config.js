4e292cf99a1a4aea2341628fea7a3c9459c45291
Ayako Nakayama <j9griffon0220@gmail.com>
2025-07-27 14:27:43 +0900

    google Fonts対応のため、Vite Plugin Webfont DLをインストール、safari対策調整中

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,18 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 import svgLoader from 'vite-svg-loader'
 
+// 25.7.26 Google Fonts対策に Vite Plugin Webfont DL を追加
+import { ViteWebfontDownload } from 'vite-plugin-webfont-dl'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     svgLoader(), // ← vite-svg-loaderを追加
     tailwindcss(), // ← Tailwindのプラグインを追加
+    ViteWebfontDownload([
+      'https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+JP:wght@300;400;500;600&family=Roboto+Mono:ital@1&display=swap',
+      ]),
   ],
   resolve: {
     alias: {
