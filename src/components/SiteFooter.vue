fec4d7b7ec88e8aeb0627aa53e9ea622c86a171c
Ayako Nakayama <j9griffon0220@gmail.com>
2025-08-09 17:12:06 +0900

    フッターにスタイルを追加（文字色、サイズなど）

--- src/components/SiteFooter.vue
+++ src/components/SiteFooter.vue
@@ -1,7 +1,9 @@
 <script setup></script>
 
 <template>
-  <footer class="font-ibm text-soft-gray text-center font-light">
+  <footer
+    class="font-ibm text-soft-gray text-calc(0.9rem+0.25vw) text-center font-light"
+  >
     &copy; Ayako Nakayama 2025
   </footer>
 </template>
