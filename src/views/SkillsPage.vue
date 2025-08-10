496e742f3390137d91a841d5fbb829492b2f1be8
Ayako Nakayama <j9griffon0220@gmail.com>
2025-08-10 14:26:31 +0900

    Skillsページのレイアウトにmin-h-screenを追加

--- src/views/SkillsPage.vue
+++ src/views/SkillsPage.vue
@@ -20,24 +20,26 @@ const selectedCategory = ref('フロントエンド')
 </script>
 
 <template>
-  <!-- 選択されたカテゴリーを表示 -->
-  <h1
-    class="font-ibm text-charcoal-gray after:bg-charcoal-gray/30 relative mb-[clamp(2.44rem,2.44rem+0.35vw,3.05rem)] text-center text-[calc(1.8rem+0.3vw)] leading-[1.2] font-semibold break-words after:absolute after:right-0 after:bottom-0 after:bottom-[-1rem] after:left-0 after:mx-auto after:h-0.5 after:w-full after:content-['']"
-  >
-    Skill・{{ selectedCategory }}
-  </h1>
-  <div class="flex h-screen min-h-0 w-full flex-1 flex-wrap">
-    <!-- 左エリア（カテゴリー選択） -->
-    <SkillsCategory
-      class="flex-shrink-0 flex-grow basis-[30ch] overflow-visible"
-      v-model:selected-category="selectedCategory"
-    />
+  <div class="min-h-screen">
+    <!-- 選択されたカテゴリーを表示 -->
+    <h1
+      class="font-ibm text-charcoal-gray after:bg-charcoal-gray/30 relative mb-[clamp(3.05rem,3.05rem+0.40vw,3.81rem)] text-center text-[calc(1.8rem+0.3vw)] leading-[1.2] font-medium break-words after:absolute after:right-0 after:bottom-0 after:bottom-[-1.5rem] after:left-0 after:mx-auto after:h-0.5 after:w-full after:content-['']"
+    >
+      Skill・{{ selectedCategory }}
+    </h1>
+    <div class="flex h-screen min-h-0 w-full flex-1 flex-wrap">
+      <!-- 左エリア（カテゴリー選択） -->
+      <SkillsCategory
+        class="flex-shrink-0 flex-grow basis-[30ch] overflow-visible"
+        v-model:selected-category="selectedCategory"
+      />
 
-    <!-- 右エリア（スキル詳細） -->
-    <SkillsDetails
-      class="flex-shrink-0 flex-grow basis-[30ch] overflow-auto"
-      :selected-category="selectedCategory"
-    />
+      <!-- 右エリア（スキル詳細） -->
+      <SkillsDetails
+        class="flex-shrink-0 flex-grow basis-[30ch] overflow-auto"
+        :selected-category="selectedCategory"
+      />
+    </div>
   </div>
 </template>
 
