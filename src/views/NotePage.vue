1e8b01d6d12c6da4f5cda43d39cbc2ff99987eba
Ayako Nakayama <j9griffon0220@gmail.com>
2025-08-11 15:21:41 +0900

    noteページを使って、左右２分割の右のみスクロール可をテスト調整

--- src/views/NotePage.vue
+++ src/views/NotePage.vue
@@ -12,14 +12,36 @@ useHead({
 })
 </script>
 
+<!-- 「左右並びのときだけ右側をスクロール可能にしたい」ことを確認するためのコード -->
 <template>
-  <div>
-    <!-- 左エリアの内容 -->
-    <templat>
-      <h1>Note</h1>
-    </templat>
+  <div class="h-screen">
+    <h1
+      class="font-ibm text-charcoal-gray after:bg-charcoal-gray/30 relative mb-[clamp(3.05rem,3.05rem+0.40vw,3.81rem)] text-center text-[calc(1.8rem+0.3vw)] leading-[1.2] font-medium break-words after:absolute after:right-0 after:bottom-0 after:bottom-[-1.5rem] after:left-0 after:mx-auto after:h-0.5 after:w-full after:content-['']"
+    >
+      Sample・Title
+    </h1>
 
-    <!-- 左エリアの内容 -->
-    <template> </template>
+    <div class="flex min-h-0 w-full flex-1 basis-[30ch] flex-wrap bg-gray-200">
+      <!-- 左エリアの内容 -->
+      <p class="flex-shrink-0 flex-grow basis-[30ch]">
+        左○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○
+      </p>
+      <!-- 右エリアの内容 -->
+      <div
+        class="h-[200px] flex-shrink-0 flex-grow basis-[30ch] overflow-auto bg-gray-100"
+      >
+        <!-- 長いコンテンツ -->
+        <p>1</p>
+        <p>2</p>
+        <p>3</p>
+        <p>4</p>
+        <p>5</p>
+        <p>6</p>
+        <p>7</p>
+        <p>8</p>
+        <p>9</p>
+        <p>10</p>
+      </div>
+    </div>
   </div>
 </template>
